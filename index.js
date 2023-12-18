08f587e8d2471c3df9565a59b646116f1612531b
sangjun park <highjun10170@gmail.com>
2023-12-18 10:37:20 +0900

    Add GithubAction Trial-0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import App from './react-native/App';
+import {name as appName} from './react-native/app.json';
 
 AppRegistry.registerComponent(appName, () => App);

d9b18ae9a66cd663e03a9d6b69e48aad5babc551
sangjun park <highjun10170@gmail.com>
2024-01-04 00:48:44 +0900

    I hope this Work

--- setup.js
+++ setup.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 try {
   // Read the contents of x.json
-  const log_stdout = process.stdout;
+  const github_output = process.env.GITHUB_OUTPUT;
   const rawData = fs.readFileSync("android/app/version.json");
   const jsonData = JSON.parse(rawData);
 
@@ -11,12 +11,12 @@ try {
   const versionName = jsonData.versionName;
 
 // //   // Output the extracted values
-//   log_stdout.write(`Version Code: ${versionCode}`);
-//   log_stdout.write(`Version Name: ${versionName}`);
+  console.log(`Version Code: ${versionCode}`);
+  console.log(`Version Name: ${versionName}`);
 
   // Set the output variables for later use in the workflow
-  console.log(`{versionCode}={${versionCode}}`);
-  console.log(`{versionName}={${versionName}}`);
+  fs.appendFileSync(github_output,`versionCode=${versionCode}`);
+  fs.appendFileSync(github_output,`versionName=${versionName}`);
 } catch (error) {
   console.error('Error reading version.json:', error.message);
   process.exit(1);
